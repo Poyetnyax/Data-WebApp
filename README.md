525e89368d91c0e9065f9e771dc931c7a252c73a
Poyet <poyetnyahuku3@gmail.com>
2024-03-21 18:39:56 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Data-Webapp
+
+## Scripts
+
+To install node-modules run the following script:
+
+``npm install``
+
+To start the webapp locally run:
+
+``npm run dev``
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <><div className="nav">
+      <p>Hello we will be developing our data-webapp using react.js :) </p>
+    </div>
+    </>
+  )
+}
+
+export default App
