5a8cb7c8f8c11182100eea554dc46a4393b3b0c4
Poyet <poyetnyahuku3@gmail.com>
2024-04-24 01:03:54 +0200

    fixes to be solved

--- src/Layouts/Landing.jsx
+++ src/Layouts/Landing.jsx
@@ -1,8 +1,11 @@
 import "./Landing.css";
-import Nav from "../Components/Organisms/Nav";
+import Nav from "../Components/Organisms/Nav/Nav"
 import Action from "../Components/Molecules/Action";
 import Client from "../Components/Molecules/Client";
 import Feature from "../Components/Molecules/Feature";
+import BodySection from "../Components/Organisms/BodySection/BodySection";
+import Customers from "../Components/Organisms/Customers/Customers";
+import CommunityUpdates from "../Components/Organisms/CommunityUpdates/CommunityUpdates";
 
 function Landing() {
   return (
@@ -19,6 +22,15 @@ function Landing() {
         </div>
         <div className="feature">
           <Feature />
+        </div>
+        <div className="bodysection">
+          <BodySection/>
+        </div>
+        <div className="customers">
+          <Customers/>
+        </div>
+        <div className="communityupdates">
+          <CommunityUpdates/>
         </div>
       </section>
     </div>
