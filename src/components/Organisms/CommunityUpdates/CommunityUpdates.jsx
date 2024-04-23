ab80ddb07bdd8968f818c9c3528817fc7cbfbb82
Poyet <poyetnyahuku3@gmail.com>
2024-04-24 00:41:26 +0200

    rebase fixes3

---
+++ src/components/Organisms/CommunityUpdates/CommunityUpdates.jsx
@@ -0,0 +1,39 @@
+import './CommunityUpdates.css';
+import WFH from '../../../assets/images/working-remote.png';
+import typing from '../../../assets/images/typing-pc.png';
+import pctyping from '../../../assets/images/pc-typing.png';
+
+function CommunityUpdates() {
+    return (
+      <>
+        <div className="container">
+            <div className="info">
+                <h2>Data analysis is our middle name</h2>
+                <p>Explore AnalytiX's blog for the latest insights on data-driven decision-making for small businesses like supermarkets. Discover how our software helps businesses increase membership income and make informed decisions. Join our community and stay ahead with AnalytiX!</p>
+            </div>
+            <div className="reviews">
+                <div className="insights">
+                    <img src={WFH} alt="" />
+                    <div className="text">
+                        <p>Insights Hub: Unlocking Data-Driven Success</p>
+                    </div>
+                </div>
+                <div className="insights">
+                    <img src={pctyping} alt="" />
+                    <div className="text">
+                        <p>Community Chronicles: Stories of Success</p>
+                    </div>
+                </div>
+                <div className="insights">
+                    <img src={typing} alt="" />
+                    <div className="text">
+                        <p>Navigating Growth: Strategies and Tips from the Community</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </>
+    );
+  }
+  
+  export default CommunityUpdates;
