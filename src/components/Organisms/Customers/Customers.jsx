e74ba07a1333de1c5b0cb1545252ca9a5450943f
Poyet <poyetnyahuku3@gmail.com>
2024-04-24 00:38:22 +0200

    rebase fixes

---
+++ src/components/Organisms/Customers/Customers.jsx
@@ -0,0 +1,22 @@
+
+
+function Customers() {
+    return(
+        <>
+        <div className="customers">
+            <div className="image">
+                <img src="aldi.png" alt=""/>
+            </div>
+            <div className="customers__more">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore, numquam tempora? Dolore officia praesentium numquam veritatis expedita iusto eius, explicabo alias, totam neque, voluptas vero cupiditate in? Delectus illum velit, libero, fugit rerum deleniti veritatis, inventore explicabo minima voluptate ut voluptatum dolor necessitatibus architecto magnam porro voluptatem nobis consequatur qui Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos obcaecati reprehenderit itaque iusto? Ipsum, alias illum! Ut laborum ipsa voluptatum commodi accusamus et reprehenderit nesciunt quibusdam quis eos, qui sapiente.</p>
+                <p>Eropean Grorcers Association</p>
+                <div className="companies">
+                    <img src="../../assets/images/zabka.png" alt="" className="campaniesImg" />
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Customers;
