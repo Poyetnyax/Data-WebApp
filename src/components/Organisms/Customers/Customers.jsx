d49c797ea525fdbe690fc4487e104d836dc6aed2
Poyet <poyetnyahuku3@gmail.com>
2024-04-24 00:40:33 +0200

    rebase fixes2

--- src/components/Organisms/Customers/Customers.jsx
+++ src/components/Organisms/Customers/Customers.jsx
@@ -19,12 +19,12 @@ function Customers() {
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore, numquam tempora? Dolore officia praesentium numquam veritatis expedita iusto eius, explicabo alias, totam neque, voluptas vero cupiditate in? Delectus illum velit, libero, fugit rerum deleniti veritatis, inventore explicabo minima voluptate ut voluptatum dolor necessitatibus architecto magnam porro voluptatem nobis consequatur qui Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos obcaecati reprehenderit itaque iusto? Ipsum, alias illum! Ut laborum ipsa voluptatum commodi accusamus et reprehenderit nesciunt quibusdam quis eos, qui sapiente.</p>
                 <p>Eropean Grorcers Association</p>
                 <div className="companies">
-                    <img src={zabka} width={100} height={70} alt="" className="campaniesImg" />
-                    <img src={carrefour} width={60} height={70}  alt="" className="campaniesImg" />
-                    <img src={strokrotka} width={130} height={70} alt="" className="campaniesImg" />
-                    <img src={action} width={140} height={70} alt="" className="campaniesImg" />
-                    <img src={biedronka} width={130} height={70} alt="" className="campaniesImg" />
-                    <img src={auchan} width={140} height={70} alt="" className="campaniesImg" />
+                    <img src={zabka}  alt="" className="campaniesImg" />
+                    <img src={carrefour}   alt="" className="campaniesImg" />
+                    <img src={strokrotka}  alt="" className="campaniesImg" />
+                    <img src={action}  alt="" className="campaniesImg" />
+                    <img src={biedronka}  alt="" className="campaniesImg" />
+                    <img src={auchan}  alt="" className="campaniesImg" />
                 </div>
             </div>
         </div>
