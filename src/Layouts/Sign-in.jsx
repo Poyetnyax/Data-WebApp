e80eaa02e820b65108a957ba70a4cba0839bba84
Poyet <poyetnyahuku3@gmail.com>
2024-05-16 18:28:33 +0200

    signin routing

---
+++ src/Layouts/Sign-in.jsx
@@ -0,0 +1,12 @@
+function Landing() {
+    return (
+      <div className="Sign-in">
+        <div className="input">
+            <input type="text" value="Username" />
+            <input type="text" value="Password" />
+        </div>
+      </div>
+    );
+  }
+  
+  export default Landing;
